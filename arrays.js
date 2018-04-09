c0eb3d79486d24d5a06e0a27aabbb2d8fd8ff0fe
bladeburris@gmail.com <bladeburris@gmail.com>
2018-04-09 11:50:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element) {
-  newArray = (array[0] = ...element)
-  return newArray
-}
